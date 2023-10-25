bc52e2f8c3cd4178bf648cdc193d4a996e5c09c8
Soffi Zahir <39215907+Soffi-Zahir@users.noreply.github.com>
2023-10-24 22:34:43 -0400

    Form

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,29 @@
 import { useState } from "react";
 
 const App = () => {
-
-  const [count,setCount] = useState(1);
-  return <h2>Lorem Ipsum Starter</h2>;
+  const [count, setCount] = useState(1);
+  const handleChange = (e) => setCount(e.target.value);
+  return (
+    <section className="section-center">
+      <h4>Bored of classic lorem ipsum ? </h4>
+      <form className="lorem-form">
+        <label htmlFor="amount"></label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          className=""
+          value={count}
+          step="1"
+          min="1"
+          max="8"
+          onChange={handleChange}
+        />
+        <button type="submit" className="btn">
+          Generate
+        </button>
+      </form>
+    </section>
+  );
 };
 export default App;
