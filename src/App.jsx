87c5c675b5f88e87e340d81fdabb31ac69dd48b3
Soffi Zahir <39215907+Soffi-Zahir@users.noreply.github.com>
2023-10-24 22:52:00 -0400

    OnSubmit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import { useState } from "react";
-import text from "./data";
+import data from "./data";
 const App = () => {
   const [count, setCount] = useState(1);
-  const [data, setData] = useState([]);
+  const [text, setText] = useState([]);
   const handleChange = (e) => setCount(e.target.value);
-  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (count < 1 || count > 8) {
+      console.error("Count must be between 1-8");
+      return;
+    }
+    setText(data.slice(0, count));
+  };
   return (
     <section className="section-center">
       <h4>Bored of classic lorem ipsum ? </h4>
-      <form className="lorem-form">
+      <form className="lorem-form" onSubmit={handleSubmit}>
         <label htmlFor="amount"></label>
         <input
           type="number"
